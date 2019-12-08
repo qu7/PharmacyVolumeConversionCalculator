f77c2c8d28372e6d53920035cbf63d0c8fbe942f
Travis Crabtree <48073815+qu7@users.noreply.github.com>
2019-12-08 09:54:35 -0600

    PR Issue #4 - exception handling
    Displays a message if number is not valid

--- PharmacyVolumeConversionCalculator/src/Window.java
+++ PharmacyVolumeConversionCalculator/src/Window.java
@@ -9,6 +9,7 @@ import java.awt.Dimension;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
 import javax.swing.JComboBox;
+import javax.swing.JPanel;
 
 public class Window extends JFrame {
 	private JTextField txtStartValue;
@@ -18,76 +19,87 @@ public class Window extends JFrame {
 		getContentPane().setLayout(null);
 		
 		txtStartValue = new JTextField();
-		txtStartValue.setBounds(10, 37, 86, 20);
+		txtStartValue.setBounds(10, 67, 86, 20);
 		getContentPane().add(txtStartValue);
 		txtStartValue.setColumns(10);
 
 		final JComboBox cmboUnitsOfMeasurement = new JComboBox(unitOfMeasurement);
-		cmboUnitsOfMeasurement.setBounds(106, 37, 139, 20);
+		cmboUnitsOfMeasurement.setBounds(106, 67, 139, 20);
 		getContentPane().add(cmboUnitsOfMeasurement);
 		
-		JLabel lblIsEqualTo = new JLabel("is equal to:");
+		JLabel lblIsEqualTo = new JLabel("is equal to: ");
 		lblIsEqualTo.setFont(new Font("Tahoma", Font.BOLD, 11));
-		lblIsEqualTo.setBounds(43, 67, 76, 14);
+		lblIsEqualTo.setBounds(43, 97, 76, 14);
 		getContentPane().add(lblIsEqualTo);
 		
 		final JLabel lblCCs = new JLabel("ccs");
-		lblCCs.setBounds(10, 92, 109, 14);
+		lblCCs.setBounds(10, 122, 109, 14);
 		getContentPane().add(lblCCs);
 		
 		final JLabel lblMLs = new JLabel("mls");
-		lblMLs.setBounds(129, 92, 109, 14);
+		lblMLs.setBounds(129, 122, 109, 14);
 		getContentPane().add(lblMLs);
 		
 		final JLabel lblTblspn = new JLabel("tblspns");
-		lblTblspn.setBounds(129, 117, 109, 14);
+		lblTblspn.setBounds(129, 147, 109, 14);
 		getContentPane().add(lblTblspn);
 		
 		final JLabel lblTspn = new JLabel("tspns");
-		lblTspn.setBounds(10, 117, 109, 14);
+		lblTspn.setBounds(10, 147, 109, 14);
 		getContentPane().add(lblTspn);
 		
 		final JLabel lblGal = new JLabel("gals");
-		lblGal.setBounds(129, 167, 109, 14);
+		lblGal.setBounds(129, 197, 109, 14);
 		getContentPane().add(lblGal);
 		
 		final JLabel lblPt = new JLabel("pts");
-		lblPt.setBounds(129, 142, 109, 14);
+		lblPt.setBounds(129, 172, 109, 14);
 		getContentPane().add(lblPt);
 		
 		final JLabel lblOz = new JLabel("ozs");
-		lblOz.setBounds(10, 142, 109, 14);
+		lblOz.setBounds(10, 172, 109, 14);
 		getContentPane().add(lblOz);
 		
 		JLabel lblTitle = new JLabel("Pharmacy Volume Conversion Calculator");
-		lblTitle.setFont(new Font("Tahoma", Font.BOLD, 14));
-		lblTitle.setBounds(10, 11, 287, 14);
+		lblTitle.setFont(new Font("Tahoma", Font.BOLD, 15));
+		lblTitle.setBounds(10, 11, 302, 20);
 		getContentPane().add(lblTitle);
 		
 		final JLabel lblLtrs = new JLabel("ltrs");
-		lblLtrs.setBounds(10, 167, 109, 14);
+		lblLtrs.setBounds(10, 197, 109, 14);
 		getContentPane().add(lblLtrs);
+		
+		final JLabel lblEnterValue = new JLabel("Enter the amount to convert:");
+		lblEnterValue.setBounds(10, 42, 235, 14);
+		getContentPane().add(lblEnterValue);
 		
 		JButton btnConvert = new JButton("Convert");
 		btnConvert.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
 				final String value = txtStartValue.getText();
-				
-				final double valueD = Double.valueOf(value);
-				final double units = cmboUnitsOfMeasurement.getSelectedIndex();
-				Calculator.calculations(valueD, units);
-				lblCCs.setText(Calculator.strCCs + " CCs");
-				lblMLs.setText(Calculator.strMLs + " MLs");
-				lblTspn.setText(Calculator.strTspn + " TSPNs");
-				lblTblspn.setText(Calculator.strTblspn + " TBLSPNs");
-				lblOz.setText(Calculator.strOz + " Oz");
-				lblPt.setText(Calculator.strPints + " Pints");
-				lblLtrs.setText(Calculator.strLtrs + " Liters");
-				lblGal.setText(Calculator.strGals + " Gallons");
+				try{
+					final double valueD = Float.parseFloat(value);
+					final double units = cmboUnitsOfMeasurement.getSelectedIndex();
+					Calculator.calculations(valueD, units);
+					lblCCs.setText(Calculator.strCCs + " CCs");
+					lblMLs.setText(Calculator.strMLs + " MLs");
+					lblTspn.setText(Calculator.strTspn + " TSPNs");
+					lblTblspn.setText(Calculator.strTblspn + " TBLSPNs");
+					lblOz.setText(Calculator.strOz + " Oz");
+					lblPt.setText(Calculator.strPints + " Pints");
+					lblLtrs.setText(Calculator.strLtrs + " Liters");
+					lblGal.setText(Calculator.strGals + " Gallons");
+					lblEnterValue.setText("Enter the amount to convert:");
+					lblEnterValue.setForeground(Color.black);
+
+				}catch (NumberFormatException ex) {
+					lblEnterValue.setText("Error! Enter a valid amount!");
+					lblEnterValue.setForeground(Color.red);
+				}
 			}
 		});
 		btnConvert.setForeground(new Color(47, 79, 79));
-		btnConvert.setBounds(255, 36, 71, 23);
+		btnConvert.setBounds(255, 66, 71, 23);
 		getContentPane().add(btnConvert);
 	}
 	
